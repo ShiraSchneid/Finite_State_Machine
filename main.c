7ee2a0c5b1f6551aac56d2c5d193eb0d1e28a692
ShiraSchneid <66646792+ShiraSchneid@users.noreply.github.com>
2020-12-28 19:40:18 -0500

    Add files via upload

--- main.c
+++ main.c
@@ -23,15 +23,16 @@ int main(int argc, char**argv) {
         printf("Error! This file could not be opened\n");       // error message
         exit(1);
     }
-    inputChars(file2);      // making sure the inputs are right
 
     printf("processing FSM definition file test1.fsm\n");
+    makeArrs(file1);                // rewinds the input file
+    rewind(file1);
     printf("FSM has %d transitions\n", numLines(file1));            // prints the number of transitions
     printf("processing FSM input file test1.inputs\n");
+    inputChars(file2);      // making sure the inputs are right
 
     rewind(file1);                  // rewinds the fsm file
     rewind(file2);
-    makeArrs(file1);                // rewinds the input file
     nextMachine(file2);      // calls nextMachine function
     rewind(file2);                  // rewinds the inputs file
     // finishes and sums up the steps and current state
@@ -45,7 +46,7 @@ int inputChars(FILE* file) {  // this reads and counts all the inputs and ensure
     int step = 0;
     while (((fgets(line, 3, file)) != NULL)) {                                              // gets each line of the text file
         if (line[0] < 'A' || (line[0] > 'Z' && line[0] < 'a') || line[0] > 'z') {           // makes sure its a letter
-            printf("There is an error in the format of your inputs!");          // error message
+            printf("----There is an error in the format of your inputs!----");          // error message
             exit(1);                                                                       // error output
         }
         step += 1;              // increments the number of steps
@@ -60,7 +61,7 @@ void nextMachine(FILE* file){        // this function switches the states based 
         printf("\tat step %d, %c", steps, letter);          // prints the current step and the input
         for (int a = 0; a < count; a++) {                           // goes through a loop of the number of inputs
             if (letter == inputArr[a] && curState == stateArr[a]) {         // if the input is the same and the current state is the same
-                printf(" transition from state %d ",curState);          // print this and the current state
+                printf(" transitions from state %d ",curState);          // print this and the current state
                 nextState = nextArr[a];                                 // update the next state to be the determined next state
                 curState = nextState;                               // update the current state to be the next state
                 break;
@@ -78,6 +79,10 @@ void makeArrs(FILE* file) {     // this function makes the fsm file into arrays.
     char input;
     int i=0;
     while(fscanf(file, "%d:%c>%d\n", &state, &input, &next) != EOF) {       // read in the lines and set them to variables
+        if (state < 0 || next < 0) {
+            printf("----One of your states is negative!----");
+            exit(1);
+        }
         stateArr[i] = state;                 // array of current states
         inputArr[i] = input;                // array of input
         nextArr[i] = next;                      // array of next state
